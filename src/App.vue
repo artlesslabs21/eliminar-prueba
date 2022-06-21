0d62012d463f93cfe8b255582ae6fde6a6ae3c30
memo <artlesslabs2@gmail.com>
2022-06-21 13:03:16 -0500

    inicie mi repositorio

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-main>
+
+      <v-toolbar app class="warning">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn class="success ml-auto">
+          Ingreso
+        </v-btn>
+        <v-btn class="error ml-auto">
+          Salir
+        </v-btn>
+      </v-toolbar>
+
+      <v-navigation-drawer app v-model="drawer" temporary dark>
+        <v-layout mt-4 column align-center>
+          <v-flex>
+            <v-avatar>
+              <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+            </v-avatar>
+          </v-flex>
+          <v-fle>
+            <p class="white--text mt-3 headline">Juanito Perez</p>
+          </v-fle>
+        </v-layout>
+      </v-navigation-drawer>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      drawer: false
+    }
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-btn>1</v-btn>
+    <v-btn>2</v-btn>
+    <v-btn>3</v-btn>
+    <v-btn>4</v-btn>
+    <v-btn>5</v-btn>
+    <v-btn>6</v-btn>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+  }
+</script>
+
+<style>
+
+.pri{
+  background-color: red;
+}
+
+.sec{
+  background-color: orange;
+}
+
+.ter{
+  background-color: darkgreen;
+}
+
+</style>
